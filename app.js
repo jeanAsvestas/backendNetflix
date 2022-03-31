e1dca7528c9bd1bcf659bc5ffa4ebbd3baa57422
jeanAsvestas <j.asbestas@gmail.com>
2022-03-31 14:24:00 +0300

    removed unused libraries and files

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// const passport = require('passport');
-// const Strategy = require('passport-local').Strategy;
-// const db = require('./models/index');
-// const bodyParser = require('body-parser');
-// const session = require('express-session');
-// const bcrypt = require('bcryptjs')
 const cors = require("cors");
 
 
@@ -20,12 +14,10 @@ var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
 var planRouter = require('./routes/plan.routes')
 var movieRouter = require('./routes/movie.routes');
-//var authRouter = require('./routes/auth.routes/auth.routes')
 var authRouter = require ('./routes/auth.routes')
 var userRouter = require ('./routes/user.routes');
 var categoryRouter = require('./routes/category.routes');
 var moviecategoryRouter = require('./routes/moviecategory.routes');
-const category = require('./models/category');
 var app = express();
 
 // view engine setup
@@ -44,20 +36,8 @@ var corsOptions = {
   origin: "http://localhost:3000"
 };
 app.use(cors(corsOptions));
-//passport
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(session({ secret: 'anything' }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/')));
-app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
-// ends passport
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
@@ -74,42 +54,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-//passport
-// passport.use(new Strategy({
-// 	usernameField: 'email',
-// 	passwordField: 'password'
-// 	},
-// 	function(username, password, cb) {
-// 		db.User.findOne({
-// 			where: {
-// 				email: username
-// 			},
-// 			raw : true
-// 		}).then(function(user){
-// 			if (!user) { return cb(null, false); }// false, no login redirect			
-// 			if (!bcrypt.compareSync(password,user.password)) { return cb(null, false); }// false, no login redirect
-// 			return cb(null, user);
-// 		}).catch(function(error){
-// 			if (error) { return cb(null, error); }
-// 	});
-// }));
-// //auth.authenticated this function returns a user.email
-// //serialize == i put something in a series of things
-// // 1 2 4 5 3
-// passport.serializeUser(function(user, cb) {
-// 	cb(null, user.email);
-// });
 
-// passport.deserializeUser(function(username, cb) {
-// 	db.User.findOne({
-// 		where: {
-// 			email: username
-// 		},
-// 		raw : true
-// 	}).then(function(user) {
-// 		cb(null, user.id);
-// 	});
-// });
 
 
 
